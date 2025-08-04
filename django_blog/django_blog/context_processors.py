4af492bc693d49f742e0d9324da42bc7388d68be
mandel <nelsonokiki@gmail.com>
2025-08-04 13:35:49 +0100

    update context processor

--- django_blog/django_blog/context_processors.py
+++ django_blog/django_blog/context_processors.py
@@ -1,9 +1,9 @@
+import datetime
 from django.conf import settings
-import time
 
 def blog_settings(request):
     return {
         'BLOG_NAME': settings.BLOG_NAME,
         'BLOG_DESCRIPTION': settings.BLOG_DESCRIPTION,
-        'YEAR': time.strftime('%Y'),
+        'YEAR': datetime.datetime.now().year,
     }
